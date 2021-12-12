9abe5621afa3f1d9045fd269f0d751ce52b3b016
slongstreet <slongstreet@hotmail.com>
2021-12-11 18:28:45 -0800

    day 06 part 2 complete.

--- day06/Program.cs
+++ day06/Program.cs
@@ -3,12 +3,15 @@
     using System;
     using System.Collections.Generic;
     using System.IO;
+    using System.Linq;
 
     public static class Program
     {
+        public const int MAXAGE = 8;
+
         static void Main(string[] args)
         {
-            string input = File.ReadAllText("input/sample.txt");
+            string input = File.ReadAllText("input/input.txt");
             List<int> fishStatus = new List<int>();
             foreach (string fish in input.Split(',', StringSplitOptions.RemoveEmptyEntries))
             {
@@ -18,8 +21,29 @@
             SimulateFishPopulation(fishStatus, 80);
             Console.WriteLine("[PART1] Fish population: {0}", fishStatus.Count);
 
-            SimulateFishPopulation(fishStatus, 256-80);
-            Console.WriteLine("[PART2] Fish population: {0}", fishStatus.Count);
+            // To avoid managing a list of billions of integers for part 2, let's just sum up how
+            // many new fish are born each day!
+            long[] fishCounter = new long[MAXAGE+1];  // all fish are 0-8 days old.
+
+            // We already have 80 days simulated, so let's reuse the data!
+            foreach (int f in fishStatus)
+            {
+                fishCounter[f]++;
+            }
+
+            // Now let's track the number of fish that should be in each status for the remaining days.
+            for (int day = 80; day < 256; ++day)
+            {
+                long born = fishCounter[0];
+                for (int j = 0; j < MAXAGE; ++j)
+                {
+                    fishCounter[j] = fishCounter[j+1];
+                }
+                fishCounter[6] += born;
+                fishCounter[MAXAGE] = born;
+            }
+
+            Console.WriteLine("[PART2] Fish population: {0}", fishCounter.Sum());
         }
 
         private static void SimulateFishPopulation(List<int> fishStatus, int iterations)
@@ -41,6 +65,8 @@
                 {
                     fishStatus.Add(8);
                 }
+
+                Console.WriteLine("{0}:{1}", i, fishStatus.Count);
             }
         }
     }
